ed30b197de237d78eba865e30e101054d300f82b
Ayat Rahman <57280365+ayat1041@users.noreply.github.com>
2023-04-03 16:51:50 +0600

    until reload

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,48 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-import { createBrowserRouter,RouterProvider } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import About from "./components/About/About";
+import Contact from "./components/Contact/Contact";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+
+// const router = createBrowserRouter([
+// {
+//   path: "/",
+//   element: <App></App>
+//   // element: <div>Hello World!</div>
+// },
+// {
+//   path: "/about",
+//   element: <About></About>
+// },
+// {
+//   path: "/contact",
+//   element: <Contact></Contact>
+// }]);
 
 const router = createBrowserRouter([
-{
-  path: "/",
-  element: <App></App>
-  // element: <div>Hello World!</div>
-}
-
+  {
+    path: "/",
+    element: <Home></Home>,
+    children: [
+      {
+        path: "about",
+        element: <About></About>,
+      },
+      {
+        path: "contact",
+        element: <Contact></Contact>,
+      },
+    ],
+  },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     {/* <App /> */}
     <RouterProvider router={router}></RouterProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
